4d6fa53c157f6a7c9aa3f8afafbdd5c027e59ad1
Vishalvaish407 <vishalvaish75@gmail.com>
2021-05-09 16:16:55 +0530

    Registration course emails

---
+++ app/Views/emails/courseRegister.php
@@ -0,0 +1,11 @@
+<h3>Hi <?=$name?>,</h3>
+
+<p><b>Course Details</b></p>
+<hr>
+<p><b>Course Name</b>: <?=$courseDetails['name']?></p>
+<p><b>Course Duration</b>: <?=$courseDetails['duration']?></p>
+<br>
+<p>Thank you for joining our course. We will connect with you soon.</p>
+<p>Till then, Stay Safe, Stay Secure in your home from COVID-19 pandemic.</p>
+<p>Thank You & Regards</p>
+<p>Vedarth Yoga</p>
